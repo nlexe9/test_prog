e8b613689341a13d12d423c104ec67ee068c582c
unknown <dfghguie@gmail.com>
2024-10-14 13:36:14 +0400

    add to do

---
+++ dice_simulator.py
@@ -0,0 +1,12 @@
+import random
+
+def dice_simulator():
+    num_dice = int(input("Enter the number of dice: "))
+    rolls = []
+
+    for _ in range(num_dice):
+        roll = random.randint(1, 6)
+        rolls.append(roll)
+    print("Rolls:", rolls)
+
+dice_simulator()
